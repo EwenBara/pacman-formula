0124ac0f8d1f5050b0cf73d4a2a9a2df55b6fbe6
Ewen BARA <ewen.bara@gmail.com>
2019-08-16 15:50:01 +0200

    ci(kitchen): add inspec files

---
+++ test/integration/default/controls/config_spec.rb
@@ -0,0 +1,12 @@
+config_file = /etc/pacman.conf
+
+control 'Pacman configuration' do
+  title 'should match desired lines'
+
+  describe file(config_file) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+  end
+end
